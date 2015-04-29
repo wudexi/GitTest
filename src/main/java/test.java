baeec5cffa3d162f5dee6ae44caa84462c474e7e
test <dexi@dexi-PC>
2015-04-30 00:23:12 +0800

    测式项目1

---
+++ src/main/java/test.java
@@ -0,0 +1,15 @@
+import org.apache.commons.lang.StringUtils;
+
+
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String a="";
+		System.out.println(StringUtils.isBlank(a));
+
+	}
+
+}
